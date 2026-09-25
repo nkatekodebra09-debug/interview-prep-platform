8db3721cd8516876156e1112dec68a5f74587f3c
Nkateko Debra Ndlovu <nkatekodebra09@gmail.com>
2026-09-25 22:12:05 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1>Interview Preparation Platform</h1>
+     <p>Frontend is running successfully</p>
+    </>
+  )
+}
+
+export default App
